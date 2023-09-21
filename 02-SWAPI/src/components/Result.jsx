33981775cc6072efa6d4066804016ac5b773e1ef
MauricioDMO <mauriciodmoprogrammer@gmail.com>
2023-09-21 13:52:52 -0600

    Implementando la imagen de fondo random

--- 02-SWAPI/src/components/Result.jsx
+++ 02-SWAPI/src/components/Result.jsx
@@ -1,19 +1,14 @@
-import './result.css'
-import { capitalize } from '../services/capitalize'
+import { useRef } from 'react'
+import { ResultText } from './ResultText'
+import { getRandomImg } from '../services/getRandomImg'
 
-export function Result ({ result, all }) {
-  const { name } = result
-  const currentEntries = Object.entries(result)
-    .filter(entry => !['name', 'created', 'edited'].includes(entry[0]))
-  return <>
-    <h2 className="name">{name}</h2>
-    {all
-      ? currentEntries.map(([prop, value]) => <p key={prop}>
-          <b>{capitalize(prop.split('_').join(' '))}:</b> {value}
-        </p>)
-      : currentEntries.slice(0, 2).map(([prop, value]) => <p key={prop}>
-      <b>{capitalize(prop.split('_').join(' '))}:</b> {value}
-    </p>)
-      }
-  </>
+export function Result ({ result, onClick }) {
+  const style = useRef(getRandomImg())
+  return <li
+  className='result'
+  onClick={onClick}
+  style={style.current}
+  >
+    <ResultText result={result} />
+  </li>
 }
--- 02-SWAPI/src/components/Results.jsx
+++ 02-SWAPI/src/components/Results.jsx
@@ -1,35 +1,44 @@
 import './results.css'
 import './modal.css'
-import { useRef, useState } from 'react'
+import { useState } from 'react'
 import { useResults } from '../hooks/useResults'
-import { Result } from './Result'
 import { timeout } from '../services/timeout'
+import { Result } from './Result'
+import { Modal } from './Modal'
 
 export function Results () {
   const { results, loading, filter, moreResults } = useResults()
   const [modal, setModal] = useState(null)
-  const modalContainer = useRef()
 
   const closeModal = async (e) => {
     if (!(e.target.className === 'modal-container' || e.target.className === 'close-modal')) return
-    modalContainer.current.classList.add('fade-out')
+    document.querySelector('.modal-container').classList.add('fade-out')
     await timeout(100)
     setModal(null)
   }
 
   return <>
   <div className='results-container'>
-    <p className='n-results' style={{ visibility: !loading || filter.page > 1 ? 'visible' : 'hidden' }}>Number of results {results.count}</p>
+    <p
+    className='n-results'
+    style={{
+      visibility: !loading || filter.page > 1
+        ? 'visible'
+        : 'hidden'
+    }}>
+      Number of results {results.count}
+    </p>
 
     <main className='main'>
       <ul className='main-results'>
         {
-          results.results.map((result) => <li
-          className='result'
-          onClick={() => setModal(result)}
-          key={result.created}>
-            <Result result={result} />
-          </li>)
+          results.results.map((result) => (
+            <Result
+              onClick={() => { setModal(result) }}
+              key={result.created}
+              result={result}
+            />
+          ))
         }
       </ul>
     </main>
@@ -41,12 +50,7 @@ export function Results () {
     <div className='loader'></div>
   }
   {modal &&
-    <div ref={modalContainer} className='modal-container' onClick={closeModal}>
-      <section className='modal'>
-        <div className='close-modal'></div>
-        <Result result={modal} all />
-      </section>
-    </div>
+    <Modal onClick={closeModal} result={modal} />
   }
 </>
 }
